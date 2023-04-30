f76ec7513a8807c1f9c29fe21ddcdc77e754031e
halimAremu <halimat3000@gmail.com>
2023-04-30 11:58:59 -0500

    Created dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask, Blueprint
-
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv
 
 def create_app(test_config=None):
     app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:postgres@localhost:5432/simons_example_db'
-    app.config['SECRET_KEY'] = 'KHGLYTDKTEASRYDT'
+    app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get("SQLALCHEMY_DATABASE_URI")
+    app.config['SECRET_KEY'] = os.environ.get("SQLALCHEMY_SECRET_KEY")
     
     db.init_app(app)
     migrate.init_app(app, db)
